f6b724e8e1d7818cc85084cea0a190fdb2991d06
Paulo Lacerda <paulodetasso@proton.me>
2023-06-06 09:37:41 -0300

    refactor: change id property type from entities and use-cases

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,12 +1,16 @@
 import { randomUUID } from "node:crypto";
 import { Slug } from "./value-objects/slug";
 import { Entity } from "../../core/entities/entity";
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 
 interface QuestionProps {
   title: string;
   content: string;
   slug: Slug;
-  authorId: string;
+  authorId: UniqueEntityID;
+  createdAt: Date
+  updatedAt?: Date
+  bestAnswerId?: UniqueEntityID
 }
 
 export class Question extends Entity<QuestionProps> {
--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,10 +1,11 @@
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 import { Answer } from "../entities/answer";
 import { AnswersRepository } from "../repositories/answers-repository";
 
 interface AnswerQuestionDTO {
   content: string;
-  instructorId: string;
-  questionId: string;
+  instructorId: UniqueEntityID;
+  questionId: UniqueEntityID;
 }
 
 export class AnswerQuestionUseCase {
@@ -14,7 +15,7 @@ export class AnswerQuestionUseCase {
 
   async execute({ content, instructorId, questionId }: AnswerQuestionDTO) {
     const answer = new Answer({
-      content, authorId: instructorId, questionId
+      content, authorId: instructorId, questionId, createdAt: new Date()
     });
 
     await this.answersRepository.create(answer);
