cbed9970a4f7b96fec0f881171f924ae4fad3a45
Paulo Lacerda <paulodetasso@proton.me>
2023-06-07 19:13:36 -0300

    feat: implements delete answer use case

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -4,7 +4,23 @@ import { AnswersRepository } from '../../src/domain/forum/application/repositori
 export class InMemoryAnswersRepository implements AnswersRepository {
   public items: Answer[] = []
 
+  async findById(id: string) {
+    const answer = this.items.find((item) => item.id.toString() === id)
+
+    if (!answer) {
+      return null
+    }
+
+    return answer
+  }
+
   async create(answer: Answer) {
     this.items.push(answer)
+  }
+
+  async delete(answer: Answer) {
+    const index = this.items.findIndex((item) => item.id === answer.id)
+
+    this.items.splice(index, 1)
   }
 }
